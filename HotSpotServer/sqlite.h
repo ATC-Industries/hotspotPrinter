2b1e82d51fe10bcbafc0fd6ab86ead3e84eb2b6a
atclarkson <atclarkson@gmail.com>
2018-12-23 19:02:39 -0500

    sql function creates a JSON object and prints it to the serial monitor

--- HotSpotServer/sqlite.h
+++ HotSpotServer/sqlite.h
@@ -11,11 +11,14 @@
 #include <sqlite3.h>
 #include <ArduinoJson.h>        // https://arduinojson.org/v5/example/generator/
 
-
-extern String results[75][9];      //array the holds sql data
+const int COL = 9;
+extern String results[75][COL];      //array the holds sql data
 extern int rec;                     //must be declared here and in main program so values xfer
 const char* data = "SQL reply";     //text to be printed when sql commands are processed
 
+ DynamicJsonBuffer jsonBuffer;
+// Build a JSON of query
+ JsonObject& root = jsonBuffer.createObject();
 
 /* This callback routine is where the return data from the query is processed
     argc = number of columns in query (integer value)
@@ -25,18 +28,20 @@ const char* data = "SQL reply";     //text to be printed when sql commands are p
 static int callback(void *data, int argc, char **argv, char **azColName) {  //function to display column name and value
   int i;
  // rec = 0;                                                                 //reset pointer
-
  // Serial.printf("%s:\n", (const char*)data);                            //print 'SQL reply' to serial monitor
   if (rec == 0)
       {Serial.printf("%s\t%s\n\r",azColName[0],azColName[1]);}          //only print the column names once
   for (i = 0; i < argc; i++) {                                             //display all the columns selected in query
       Serial.printf("%s\t", argv[i] ? argv[i] : "NULL");                   //print the field values
-    //Serial.printf("%s = %s\t", azColName[i], argv[i] ? argv[i] : "NULL");  //print column name and value then tab
-    results[rec][i] = argv[i];                                             //save data to array
-   // if (rec < 10){
-   //     recordArray[rec][i] = argv[i];
-   // }                                         //save to array
+      //Serial.printf("%s = %s\t", azColName[i], argv[i] ? argv[i] : "NULL");  //print column name and value then tab
+      results[rec][i] = argv[i];
   }
+
+  JsonArray& jsonRecord = root.createNestedArray(String(rec));
+  for (int i = 0; i < COL; i++) {
+      jsonRecord.add(results[rec][i]);
+    }
+  //root[String(rec)] = results[rec][0];
   Serial.printf("\n");                                                     //print a space between records
   rec++;                                                                   //increment record counter
 
@@ -64,6 +69,9 @@ int db_exec(sqlite3 *db, const char *sql) {                                //thi
     Serial.printf("SQL error: %s\n", zErrMsg);                         //print the error message on error
     sqlite3_free(zErrMsg);
   } else {
+      // Print out the JSON
+    root.prettyPrintTo(Serial);
+    Serial.printf("\n");
     Serial.printf("Operation done successfully\n");
   }
   Serial.printf("%d records found\n", rec);
