5c16b93ed65dc13a68e35e4576eb72b9a908d354
atclarkson <atclarkson@gmail.com>
2019-07-11 11:40:07 -0400

    Fixed form input width

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('njk', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('HotSpotServer/app/pages/**/*.+(html|nunjucks|njk)')
+  return gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
   // Renders template with nunjucks
   .pipe(nunjucksRender({
-      path: ['HotSpotServer/app/templates']
+      path: ['app/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('HotSpotServer/data'))
+  .pipe(gulp.dest('data'))
 });
